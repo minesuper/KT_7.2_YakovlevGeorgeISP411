6e7bc67b00c51702d9ae6a79744564f5e575832a
FedorProgrammer123 <fedorlohkin@gmail.com>
2024-11-18 16:43:10 +0300

    Partner page, history page, Add/Edit xaml

---
+++ MasterFloor/Pages/History.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MasterFloor.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для History.xaml
+    /// </summary>
+    public partial class History : Page
+    {
+        public History(Model.Partner CurrentPartner)
+        {
+            InitializeComponent();
+            OnStart(CurrentPartner);
+        }
+        private void OnStart(Model.Partner CurrentPartner)
+        {
+            HistoryListView.ItemsSource = Model.MasterFloorDBEntities.GetContext().PartnerProduct
+                .Where(d => d.ParnerId == CurrentPartner.Id).ToList();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Classes.Navigation.ActiveFrame.CanGoBack)
+            {
+                Classes.Navigation.ActiveFrame.GoBack();
+            }
+        }
+    }
+}
---
+++ MasterFloor/Pages/Partners.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MasterFloor.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Partners.xaml
+    /// </summary>
+    public partial class Partners : Page
+    {
+        public Partners()
+        {
+            InitializeComponent();
+            OnStart();
+        }
+        private void OnStart()
+        {
+            PartnersListView.ItemsSource = Model.MasterFloorDBEntities.GetContext().Partner.ToList();
+        }
+
+        private void EditParterButton_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Navigation.ActiveFrame.Navigate(new Pages.AddEditPartner((sender as Button).DataContext as Model.Partner));
+        }
+
+        private void HistoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Navigation.ActiveFrame.Navigate(new Pages.History((sender as Button).DataContext as Model.Partner));
+        }
+
+        private void AddPartnerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Navigation.ActiveFrame.Navigate(new Pages.AddEditPartner(null));
+        }
+    }
+}
